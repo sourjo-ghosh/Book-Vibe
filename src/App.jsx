f5f90e6549a33e78bf153f8fba5d02b30327ba8d
sourjo-ghosh <joyghosh211@outlook.com>
2026-04-08 00:42:22 +0600

    tailwindcss, daisyUI, react-router install

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
+    <div className='text-6xl text-red-500'>
       hello world
+      <button className='btn'> click me </button>
     </div>
   );
 };
